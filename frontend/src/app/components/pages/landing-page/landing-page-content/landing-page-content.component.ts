cba12c8ef517f495da201abcfc400d79e587b803
Daniel Rogowski <daniel.rogowski@onet.pl>
2024-05-30 22:40:25 +0200

    EBW-110: Create SVG illustrations for landing page

--- frontend/src/app/components/pages/landing-page/landing-page-content/landing-page-content.component.ts
+++ frontend/src/app/components/pages/landing-page/landing-page-content/landing-page-content.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {RouterModule} from "@angular/router";
 import {LandingPageNavbarComponent} from "../landing-page-navbar/landing-page-navbar.component";
 import {MaterialModule} from "../../../../modules/material.module";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
     selector: 'app-landing-page-content',
     standalone: true,
-    imports: [LandingPageNavbarComponent, MaterialModule, RouterModule],
+    imports: [LandingPageNavbarComponent, MaterialModule, RouterModule, NgOptimizedImage],
     templateUrl: './landing-page-content.component.html',
     styleUrl: './landing-page-content.component.scss'
 })
