3789616fe95b2d524b373ec41b10e1b23435aef3
Daniel Rogowski <daniel.rogowski@onet.pl>
2024-06-01 20:47:21 +0200

    EBW-17: Create empty Spring project for backend

---
+++ backend/src/main/java/com/github/danrog303/ebookwizard/EbookWizardApplication.java
@@ -0,0 +1,13 @@
+package com.github.danrog303.ebookwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookWizardApplication.class, args);
+	}
+
+}
