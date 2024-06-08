09c2761310c5895e29afb87bc35f810da460eb65
Daniel Rogowski <daniel.rogowski@onet.pl>
2024-06-08 23:18:42 +0200

    EBW-57: Expose AWS S3 beans

---
+++ backend/src/main/java/com/github/danrog303/ebookwizard/external/storage/FileStorageService.java
@@ -0,0 +1,11 @@
+package com.github.danrog303.ebookwizard.external.storage;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.stream.Stream;
+
+public interface FileStorageService {
+    void uploadFile(String key, File file);
+    InputStream downloadFile(String key);
+    void deleteFile(String key);
+}
---
+++ backend/src/main/java/com/github/danrog303/ebookwizard/external/storage/s3/AwsS3FileStorageService.java
@@ -0,0 +1,52 @@
+package com.github.danrog303.ebookwizard.external.storage.s3;
+
+import com.github.danrog303.ebookwizard.external.storage.FileStorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+public class AwsS3FileStorageService implements FileStorageService {
+    private final S3Client s3Client;
+
+    @Value("${amazon.aws.s3.bucket-name}")
+    private String bucketName;
+
+    @Override
+    public void uploadFile(String key, File file) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromFile(file));
+    }
+
+    @Override
+    public InputStream downloadFile(String key) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        return s3Client.getObject(getObjectRequest);
+    }
+
+    @Override
+    public void deleteFile(String key) {
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+    }
+}
